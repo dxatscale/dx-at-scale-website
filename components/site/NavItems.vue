c437993ee7e4a61b5e0a1ac4182f1fc6f680d8e9
Ramzi Akremi <ramzi.dev@icloud.com>
2022-08-30 01:10:41 +1000

    chore(ui): tweak navbar

--- components/site/NavItems.vue
+++ components/site/NavItems.vue
@@ -1,6 +1,6 @@
 <template>
   <li v-for="item in navigation">
-    <a :href="item.href" :key="item.name" class="hover:text-sky-500 dark:hover:text-sky-400">{{ item.name }}</a>
+    <a :href="item.href" :key="item.name" class="text-base block px-3 md:px-0 py-2 hover:text-pink-500 dark:hover:text-pink-400">{{ item.name }}</a>
   </li>
 </template>
 
