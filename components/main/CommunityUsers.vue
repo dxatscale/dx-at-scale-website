05dc4b889bcb8fe3d444cf31a8f4c6fc49cc15f9
Caitlyn Mills <63215232+Caitlyn-Mills@users.noreply.github.com>
2022-09-16 15:13:35 +0930

    feat(communityusers): add community user logos
    Add logos from valantic, expereo, k-health

--- components/main/CommunityUsers.vue
+++ components/main/CommunityUsers.vue
@@ -4,13 +4,13 @@
       <p class="py-12 text-center text-base font-semibold uppercase text-pink-600 dark:text-pink-400 tracking-wider">DX@Scale is used here</p>
       <div class="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-8">
         <div class="col-span-1 flex justify-center py-8 px-8 bg-slate-100 dark:bg-slate-800">
-          <img class="max-h-12" src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg" alt="Workcation" />
+          <img class="max-h-12" src="~/assets/img/logos/valantic.svg" alt="Valantic" />
         </div>
         <div class="col-span-1 flex justify-center py-8 px-8 bg-slate-100 dark:bg-slate-800">
-          <img class="max-h-12" src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg" alt="Mirage" />
+          <img class="max-h-12" src="~/assets/img/logos/expereo.png" alt="expereo" />
         </div>
         <div class="col-span-1 flex justify-center py-8 px-8 bg-slate-100 dark:bg-slate-800">
-          <img class="max-h-12" src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg" alt="Tuple" />
+          <img class="max-h-12" src="~/assets/img/logos/k-health.png" alt="k-health" />
         </div>
         <div class="col-span-1 flex justify-center py-8 px-8 bg-slate-100 dark:bg-slate-800">
           <img class="max-h-12" src="https://tailwindui.com/img/logos/laravel-logo-gray-400.svg" alt="Laravel" />
