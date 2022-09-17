1dcdaf0e91d747639fb400ec68c5d5ebeda5a84b
azlam-abdulsalam <azlam.abdulsalam@accenture.com>
2022-09-17 20:57:52 +1000

    feat(community):add links to users

--- components/main/CommunityUsers.vue
+++ components/main/CommunityUsers.vue
@@ -3,15 +3,21 @@
     <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
       <p class="py-12 text-center text-base font-semibold uppercase text-pink-600 dark:text-pink-400 tracking-wider">DX@Scale is used here</p>
       <div class="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-8">
+        <a href="https://valantic.com">
         <div class="col-span-1 flex justify-center py-8 px-8 bg-slate-100 dark:bg-slate-600">
-          <img class="max-h-12" src="~/assets/img/logos/valantic.svg" alt="Valantic" />
+          <img class="max-h-12" src="~/assets/img/logos/valantic.svg" alt="Valantic"  />
         </div>
+        </a>
+        <a href="https://www.expereo.com/">
         <div class="col-span-1 flex justify-center py-8 px-8 bg-slate-100 dark:bg-slate-600">
           <img class="max-h-12" src="~/assets/img/logos/expereo.png" alt="expereo" />
         </div>
+        </a>
+        <a href="https://www.khealth.com/">
         <div class="col-span-1 flex justify-center py-8 px-8 bg-slate-100 dark:bg-slate-600">
           <img class="max-h-12" src="~/assets/img/logos/k-health.png" alt="k-health" />
         </div>
+       </a>
       </div>
     </div>
   </div>
