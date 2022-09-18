0039de35eb22fc6711f85d31fd010d3f6873a2a2
Ramzi Akremi <ramzi.dev@icloud.com>
2022-09-18 16:54:01 +1000

    chore: format nuxt.config.js file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,12 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt/config";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: [
-        '@nuxtjs/tailwindcss',
-        '@nuxt/content'
-    ],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/content"],
 
-    css: [
-        '~/assets/css/fonts.css'
-      ],
+  css: ["~/assets/css/fonts.css"],
 
-      build: {
-        transpile: [
-          "@heroicons/vue",
-        ]
-      }
-})
+  build: {
+    transpile: ["@heroicons/vue"],
+  },
+});
