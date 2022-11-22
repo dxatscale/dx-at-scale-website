0177106571ab29ee73f14e208d39f9efbec4c4ca
Ramzi Akremi <ramzi.dev@icloud.com>
2022-11-22 17:28:01 +1100

    feat: add animation to why dx@scale

--- components/main/Why.vue
+++ components/main/Why.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="py-24 px-8 bg-slate-100 dark:bg-slate-800 text-center">
     <div class="max-w-7xl mx-auto ">
-      <h2 class="py-12 text-center text-base font-semibold uppercase text-pink-600 dark:text-pink-400 tracking-wider">
+      <h2 class="py-12 text-center text-base font-semibold uppercase text-pink-600 dark:text-pink-400 tracking-wider" v-motion-slide-visible-bottom>
         Simplify Salesforce Developer Experience at any scale
       </h2>
       <figure>
-        <blockquote>
+        <blockquote  v-motion-fade-visible>
           <p class="mt-6 max-w-3xl mx-auto text-lg text-slate-900 dark:text-white/90">
             We had few sessions
             <a href="https://docs.dxatscale.io/talks-on-dx-scale"
@@ -14,8 +14,8 @@
             </a>
             where we talked about our approach to building modular Salesforce Orgs.
             We found out that if we focus on sane Software Engineering principles
-            like modularisation, trunk based development , continuous integration 
-            and the right tooling, we can build and maintain any org at any scale, 
+            like modularisation, trunk based development , continuous integration
+            and the right tooling, we can build and maintain any org at any scale,
             with consistency, confidence and moreover high level of quality.
           </p>
           <p class="mt-6 max-w-3xl mx-auto text-lg text-slate-900 dark:text-white/90">
@@ -23,12 +23,12 @@
           </p>
         </blockquote>
         <figcaption class="mt-8 flex justify-center space-x-4 text-center">
-          <div class="w-48">
+          <div class="w-48" v-motion-pop-visible>
             <img src="~/assets/img/avatars/azlam.jpg" alt="" class="mx-auto w-24 h-24 rounded-full" loading="lazy" />
             <div class="mt-4 text-slate-900 dark:text-white">Azlam</div>
             <div class="text-sm text-slate-900 dark:text-white/70">Program Steward, DX@Scale</div>
           </div>
-          <div class="w-48">
+          <div class="w-48" v-motion-pop-visible>
             <img src="~/assets/img/avatars/ramzi.jpg" alt="" class="mx-auto w-24 h-24 rounded-full" loading="lazy" />
             <div class="mt-4 text-slate-900 dark:text-white">Ramzi</div>
             <div class="text-sm text-slate-900 dark:text-white/70">Software Engineering Lead</div>
