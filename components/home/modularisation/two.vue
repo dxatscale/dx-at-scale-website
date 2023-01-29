c64333094d66ae50844e78b655658a33caabb869
Ramzi Akremi <ramzi.dev@icloud.com>
2023-01-29 15:57:06 +1100

    fix: fix indentation

--- components/home/modularisation/two.vue
+++ components/home/modularisation/two.vue
@@ -120,7 +120,7 @@
         </div>
 
         <div class="flex py-1">
-          <svg class="mr-1 ml-12 w-4 text-slate-400" viewBox="0 0 20 20" fill="currentColor">
+          <svg class="mr-1 ml-8 w-4 text-slate-400" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
           </svg>
           <svg class="mr-1 w-4" viewBox="0 0 16 16"><path fill="currentColor" d="M4 8a1 1 0 1 1-2 0a1 1 0 0 1 2 0zm5 0a1 1 0 1 1-2 0a1 1 0 0 1 2 0zm5 0a1 1 0 1 1-2 0a1 1 0 0 1 2 0z" /></svg>
