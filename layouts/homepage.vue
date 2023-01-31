2f307c1b755de7f803637f52d8791101d8859ffa
Ramzi Akremi <ramzi.dev@icloud.com>
2023-01-31 21:19:13 +1100

    feat: add favicon

--- layouts/homepage.vue
+++ layouts/homepage.vue
@@ -14,8 +14,22 @@ useHead({
       content: 'width=device-width, initial-scale=1, maximum-scale=1',
     },
   ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: '/favicon.svg',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png',
+    },
+  ],
   bodyAttrs: {
     class: 'bg-white dark:bg-slate-900',
   },
 })
+// <link rel="icon" type="image/svg+xml" href="/assets/images/favicon.svg">
+// <link rel="icon" type="image/png" href="/assets/images/favicon.png">
 </script>
