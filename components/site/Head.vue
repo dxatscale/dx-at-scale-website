c6e3c2e2c92d7e8e7282ffb24e1678c10b457d85
Ramzi Akremi <ramzi.dev@icloud.com>
2023-01-31 17:49:50 +1100

    build: prepare replacement

--- components/site/Head.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <Head>
-    <Title>DX@Scale</Title>
-    <Meta name="description" content="DX@Scale" />
-    <Meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover" />
-  </Head>
-</template>
--- components/site/Logo.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <a href="#">
-    <span class="sr-only">DX@Scale</span>
-    <img class="h-8 w-auto sm:h-10" src="~/assets/img/logos/color.svg" alt="">
-  </a>
-</template>
