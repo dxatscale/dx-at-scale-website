70a9da9c7316202416d90d990560a525d830744c
Ramzi Akremi <ramzi.dev@icloud.com>
2023-02-07 15:16:14 +1100

    fix: change link to release notes

--- components/announcement.vue
+++ components/announcement.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <a href="https://medium.com/@gnemiq/declarative-configuration-for-field-history-tracking-d63525a5e7b2" class="group inline-flex space-x-4" target="_blank">
+    <a href="https://github.com/dxatscale/sfpowerscripts/releases/tag/%40dxatscale%2Fsfpowerscripts%4020.19.6" class="group inline-flex space-x-4" target="_blank">
       <span class="rounded bg-sky-100 px-2.5 py-1 text-sm text-sky-600 dark:bg-sky-600/20 dark:text-sky-400">
         <span class="hidden sm:inline-block">January</span>
         <span class="sm:hidden">Jan.</span>
