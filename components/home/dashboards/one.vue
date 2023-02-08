ef6557de2ab5cc0acfd79135457d714092fdee34
Ramzi Akremi <ramzi.dev@icloud.com>
2023-02-08 11:06:01 +1100

    build: initiate dashboard section

---
+++ components/home/dashboards/one.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mx-auto mt-12 max-w-7xl lg:grid lg:min-h-[580px] lg:grid-cols-12 lg:gap-8">
+    <div class="col-span-12 px-4 py-16 lg:col-span-5">
+      <div class="prose relative leading-normal text-slate-700 dark:text-slate-300 lg:pr-4">
+        <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, enim illo repudiandae veritatis voluptas incidunt voluptate deserunt, maxime ut dolores reiciendis corporis, labore aliquid laborum numquam fuga. Laboriosam, dicta expedita?</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde hic dolor reiciendis corporis? Debitis unde necessitatibus accusamus! Exercitationem maxime non ea dolore nobis quibusdam! Eaque qui voluptas aliquam inventore iste?</p>
+      </div>
+
+      <a class="group mt-6 inline-flex max-w-fit items-center rounded-full bg-slate-300/50 px-4 py-2 text-sm font-semibold text-slate-900 hover:bg-slate-400/50 dark:bg-slate-600/50 dark:text-slate-200 dark:hover:bg-slate-500/50" href="https://docs.dxatscale.io/sfpowerscripts/prepare#fetching-scratch-orgs-from-pool" target="_blank">
+        Learn more
+        <chevron-right class="ml-2 h-5 w-5" />
+      </a>
+    </div>
+    <div class="relative col-span-12 pb-12 lg:col-span-7">
+      <div class="lg:absolute lg:inset-0 lg:-mt-12 lg:w-[810px]"><home-dashboards-sample-one /></div>
+    </div>
+  </div>
+</template>
---
+++ components/icon/statistics.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex h-16 w-16 items-center justify-center rounded-full">
+    <svg xmlns="http://www.w3.org/2000/svg" height="100%" width="100%" viewBox="0 0 65 56">
+      <g fill="none" fill-rule="evenodd">
+        <path fill="currentColor" fill-opacity=".3" d="M40.189 20.563c0 11.012-8.927 19.94-19.939 19.94-11.013 0-19.941-8.928-19.941-19.94C.309 9.55 9.237.622 20.25.622c11.012 0 19.939 8.928 19.939 19.941Z" />
+        <path fill="currentColor" fill-opacity=".1" d="M57.318 14.622h-49a2 2 0 0 0-2 2v33h53v-33a2 2 0 0 0-2-2Z" />
+        <path stroke="currentColor" stroke-width="2" d="M6.318 49.622v-33a2 2 0 0 1 2-2h49a2 2 0 0 1 2 2v33" />
+        <path fill="currentColor" fill-opacity=".1" d="M63.318 49.622H39.319l-1.001 1h-12l-1-1h-24v2c0 1.65 1.35 3 3 3h56a3 3 0 0 0 3-3v-2Z" />
+        <path stroke="currentColor" stroke-width="2" d="M63.318 49.622H39.319l-1.001 1h-12l-1-1h-24v2c0 1.65 1.35 3 3 3h56a3 3 0 0 0 3-3v-2Z" />
+        <path fill="currentColor" fill-opacity=".7" d="M16.318 46.622h5v-7h-5zm9 0h5v-12h-5zm9 0h5v-23h-5zm9 0h5v-16h-5z" />
+        <path stroke="currentColor" stroke-width="2" d="M16.318 46.622v-7h5v7m4 0v-12h5v12m4 0v-23h5v23m4 0v-16h5v16" />
+      </g>
+    </svg>
+  </div>
+</template>
