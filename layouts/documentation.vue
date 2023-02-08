1c97d06dd3917507978b47f7b50cff4d9475e94d
Ramzi Akremi <ramzi.dev@icloud.com>
2023-02-09 08:32:00 +1100

    build: initiate documentation template

---
+++ layouts/documentation.vue
@@ -0,0 +1,6 @@
+<template>
+  <main-nav />
+  <main class="doc">
+    <slot />
+  </main>
+</template>
---
+++ pages/doc.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>Test</div>
+</template>
+<script setup>
+definePageMeta({
+  layout: 'documentation',
+})
+</script>
