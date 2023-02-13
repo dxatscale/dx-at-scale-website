98ea918fae28b7a9ab2afa53ff6d6c46e53d0ec5
Ramzi Akremi <ramzi.dev@icloud.com>
2023-02-14 08:00:35 +1100

    build: display navigation between documentation articles

--- pages/docs/[...}slug.vue
+++ pages/docs/[...}slug.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+const { path } = useRoute()
+const { data } = await useAsyncData(`content-${path}`, async () => {
+  let article = queryContent().where({ _path: path }).findOne()
+  let surround = queryContent().only(['_path', 'title', 'description']).sort({ date: 1 }).findSurround(path)
+
+  return {
+    article: await article,
+    surround: await surround,
+  }
+})
+const [prev, next] = data.value?.surround
+</script>
+
 <template>
   <main-nav />
   <div class="doc mx-auto max-w-8xl px-4 sm:px-6 md:px-8">
@@ -15,28 +29,7 @@
         <div id="content-wrapper" class="dark:prose-dark prose prose-slate relative mt-12">
           <ContentDoc />
         </div>
-        <footer class="mt-12 text-sm leading-6">
-          <div class="mb-10 flex items-center font-semibold text-slate-700 dark:text-slate-200">
-            <!-- Previous article -->
-            <a class="group flex items-center hover:text-slate-900 dark:hover:text-white" href="/docs/user-select">
-              <svg viewBox="0 0 3 6" class="mr-3 h-1.5 w-auto overflow-visible text-slate-400 group-hover:text-slate-600 dark:group-hover:text-slate-300"><path d="M3 0L0 3L3 6" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
-              User Select
-            </a>
-            <!-- Next article -->
-            <a class="group ml-auto flex items-center hover:text-slate-900 dark:hover:text-white" href="/docs/fill">
-              Fill
-              <svg viewBox="0 0 3 6" class="ml-3 h-1.5 w-auto overflow-visible text-slate-400 group-hover:text-slate-600 dark:group-hover:text-slate-300"><path d="M0 0L3 3L0 6" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
-            </a>
-          </div>
-          <!-- Footer -->
-          <div class="justify-between border-t border-slate-200 pt-10 pb-28 text-slate-500 dark:border-slate-200/5 sm:flex">
-            <div class="mb-6 sm:mb-0 sm:flex">
-              <p>Copyright © 2023 DX@Scale</p>
-              <p class="sm:ml-4 sm:border-l sm:border-slate-200 sm:pl-4 dark:sm:border-slate-200/5"><a class="hover:text-slate-900 dark:hover:text-slate-400" href="#">Brand</a></p>
-            </div>
-            <a class="hover:text-slate-900 dark:hover:text-slate-400" href="#">Edit this page on GitHub</a>
-          </div>
-        </footer>
+        <docs-footer :prev="prev" :next="next" />
 
         <!-- Aside -->
         <div class="fixed top-[3.8125rem] bottom-0 right-[max(0px,calc(50%-45rem))] hidden w-[19.5rem] overflow-y-auto py-10 xl:block">
