5ccfeef1a984154d3155d99ddd877dc2bd5dbb2b
Ramzi Akremi <ramzi.dev@icloud.com>
2023-02-14 22:21:49 +1100

    fix: fix prose components

--- components/content/ProseH4.vue
+++ components/content/ProseH4.vue
@@ -7,7 +7,7 @@
 <script setup lang="ts">
 import { useRuntimeConfig } from '#imports'
 defineProps<{ id?: string }>()
-const heading = 2
+const heading = 4
 const { anchorLinks } = useRuntimeConfig().public.content
 const generate = anchorLinks?.depth >= heading && !anchorLinks?.exclude.includes(heading)
 </script>
