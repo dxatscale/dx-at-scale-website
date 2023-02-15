a1705aa370dc30caa0a6c0c545ca1c945547d53e
Ramzi Akremi <ramzi.dev@icloud.com>
2023-02-16 08:15:04 +1100

    feat(seo): add meta information to homepage

--- layouts/homepage.vue
+++ layouts/homepage.vue
@@ -8,28 +8,11 @@
 
 <script setup>
 useHead({
-  meta: [
-    {
-      name: 'viewport',
-      content: 'width=device-width, initial-scale=1, maximum-scale=1',
-    },
-  ],
+  meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' }],
   link: [
-    {
-      rel: 'icon',
-      type: 'image/svg+xml',
-      href: '/favicon.svg',
-    },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      href: '/favicon.png',
-    },
+    { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+    { rel: 'icon', type: 'image/png', href: '/favicon.png' },
   ],
-  bodyAttrs: {
-    class: 'bg-white dark:bg-slate-900',
-  },
+  bodyAttrs: { class: 'bg-white dark:bg-slate-900' },
 })
-// <link rel="icon" type="image/svg+xml" href="/assets/images/favicon.svg">
-// <link rel="icon" type="image/png" href="/assets/images/favicon.png">
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,33 @@
+<script setup>
+const title = 'DX@Scale'
+const description = 'Used on more than 150 Salesforce orgs worldwide, DX@Scale helps you unlock the full potential of your Salesforce Org with modularisation techniques and a declarative orchestrator. Streamline your Salesforce application lifecycle by enhancing the experience of your developers and admins.'
+const shortDescription = 'Modular Salesforce Development powered by open source tools'
+const baseUrl = 'https://dxatscale.io'
+const imageUrl = ''
+
+definePageMeta({
+  layout: 'homepage',
+})
+useHead({
+  title: title,
+  meta: [
+    { hid: 'description', name: 'description', content: description },
+    { hid: 'ogSiteName', name: 'og:site_name', content: 'DX@Scale' },
+    { hid: 'ogTitle', name: 'og:title', content: shortDescription },
+    { hid: 'ogUrl', name: 'og:url', content: baseUrl },
+    { hid: 'ogType', name: 'og:type', content: 'website' },
+    { hid: 'ogDescription', name: 'og:description', content: description },
+    { hid: 'ogImage', name: 'og:image', content: imageUrl },
+    { hid: 'twitterTitle', name: 'twitter:title', content: shortDescription },
+    { hid: 'twitterUrl', name: 'twitter:creator', content: '@dxatscale' },
+    { hid: 'twitterSite', name: 'twitter:site', content: '@dxatscale' },
+    { hid: 'twitterType', name: 'twitter:type', content: 'website' },
+    { hid: 'twitterDescription', name: 'twitter:description', content: description },
+    { hid: 'twitterImage', name: 'twitter:image', content: imageUrl },
+  ],
+})
+</script>
+
 <template>
   <home-hero />
   <home-community-users />
@@ -15,9 +45,3 @@
   <home-dashboards />
   <home-pools />
 </template>
-
-<script setup>
-definePageMeta({
-  layout: 'homepage',
-})
-</script>
