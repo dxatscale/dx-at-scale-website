e5b06d5f3c7699c6bb23ef27470472eadc92608a
Ramzi Akremi <ramzi.dev@icloud.com>
2023-02-18 02:19:48 +1100

    fix(docs): fix tailwind jit issue with aspect classes

--- components/content/prose-video.vue
+++ components/content/prose-video.vue
@@ -6,12 +6,10 @@ const props = withDefaults(
   }>(),
   { videoId: '', aspectRatio: '6:9' }
 )
-
-const [width, height] = props.aspectRatio?.split(':')
 </script>
 
 <template>
-  <div :class="`aspect-w-[${width}] aspect-h-[${height}]`">
+  <div :class="{ 'aspect-w-16 aspect-h-9': aspectRatio === '16:9', 'aspect-w-4 aspect-h-3': aspectRatio === '4:3' }">
     <iframe class="embed-responsive-item absolute top-0 right-0 bottom-0 left-0 h-full w-full" :src="`https://www.youtube.com/embed/${videoId}`" allow="fullscreen" data-gtm-yt-inspected-2340190_699="true" id="240632615"></iframe>
   </div>
 </template>
