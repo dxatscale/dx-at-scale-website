920c64139206a902dc781f0fac91d20739b298c0
Ramzi Akremi <ramzi.dev@icloud.com>
2023-02-20 18:11:04 +1100

    fix(theme): display theme properly

--- components/theme-toggler.vue
+++ components/theme-toggler.vue
@@ -19,10 +19,6 @@ const { theme, setTheme, getTheme } = useTheme()
 
 const selectedTheme = computed(() => theme.value.charAt(0).toUpperCase() + theme.value.slice(1))
 
-onMounted(() => {
-  selectedTheme.value = getTheme()
-})
-
 const changeTheme = (value: string) => {
   setTheme(value)
 }
