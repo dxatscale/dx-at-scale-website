149b1b20dc7aeea5dd0daff8a371f113c2e2afd5
Ramzi Akremi <ramzi.dev@icloud.com>
2023-03-04 09:59:00 +1100

    feat: announce feb 23 release

--- components/announcement.vue
+++ components/announcement.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <a href="https://github.com/dxatscale/sfpowerscripts/releases/tag/%40dxatscale%2Fsfpowerscripts%4020.19.6" class="group inline-flex space-x-4" target="_blank">
+    <a href="https://github.com/dxatscale/sfpowerscripts/releases/tag/%40dxatscale%2Fsfpowerscripts%4020.26.2" class="group inline-flex space-x-4" target="_blank">
       <span class="rounded bg-sky-100 px-2.5 py-1 text-sm text-sky-600 dark:bg-sky-600/20 dark:text-sky-400">
-        <span class="hidden sm:inline-block">January</span>
-        <span class="sm:hidden">Jan.</span>
+        <span class="hidden sm:inline-block">February 2023</span>
+        <span class="sm:hidden">Feb. 23</span>
       </span>
       <span class="inline-flex items-center space-x-1 text-sm text-sky-600 dark:text-sky-400">
-        <span>Declarative Field History Tracking</span>
+        <span>It's all about quality of life improvements!</span>
         <chevron-right class="h-5 w-5" />
       </span>
     </a>
