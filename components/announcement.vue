5ac814d9577a751a427ca2d4ed407da2c855cfe9
Azlam <43767972+azlam-abdulsalam@users.noreply.github.com>
2023-04-06 20:41:35 +1000

    chore: announce march release

--- components/announcement.vue
+++ components/announcement.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <a href="https://github.com/dxatscale/sfpowerscripts/releases/tag/%40dxatscale%2Fsfpowerscripts%4020.26.2" class="group inline-flex space-x-4" target="_blank">
+    <a href="https://github.com/dxatscale/dxatscale#march-23" class="group inline-flex space-x-4" target="_blank">
       <span class="rounded bg-sky-100 px-2.5 py-1 text-sm text-sky-600 dark:bg-sky-600/20 dark:text-sky-400">
-        <span class="hidden sm:inline-block">February 2023</span>
+        <span class="hidden sm:inline-block">March 2023</span>
         <span class="sm:hidden">Feb. 23</span>
       </span>
       <span class="inline-flex items-center space-x-1 text-sm text-sky-600 dark:text-sky-400">
-        <span>It's all about quality of life improvements!</span>
+        <span>Work continues on release config</span>
         <chevron-right class="h-5 w-5" />
       </span>
     </a>
