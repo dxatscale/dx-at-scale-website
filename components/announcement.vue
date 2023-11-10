e8a84f52617699d8b720b2ff6b7c7ab62596cc89
Azlam <43767972+azlam-abdulsalam@users.noreply.github.com>
2023-11-10 19:31:10 +1100

    fix: update to October 23

--- components/announcement.vue
+++ components/announcement.vue
@@ -2,7 +2,7 @@
   <div>
     <a href="https://github.com/dxatscale/dxatscale#october-23" class="group inline-flex space-x-4" target="_blank">
       <span class="rounded bg-sky-100 px-2.5 py-1 text-sm text-sky-600 dark:bg-sky-600/20 dark:text-sky-400">
-        <span class="hidden sm:inline-block">August 2023</span>
+        <span class="hidden sm:inline-block">October 2023</span>
         <span class="sm:hidden">october-23</span>
       </span>
       <span class="inline-flex items-center space-x-1 text-sm text-sky-600 dark:text-sky-400">
