8c9b69a107c30f43b7daeaad839a3e0a46cb2bf5
crazynammer <crazynammer@users.noreply.github.com>
2023-12-04 17:19:47 -0800

    Update December 2023 release

--- components/announcement.vue
+++ components/announcement.vue
@@ -2,11 +2,11 @@
   <div>
     <a href="https://github.com/dxatscale/dxatscale#october-23" class="group inline-flex space-x-4" target="_blank">
       <span class="rounded bg-sky-100 px-2.5 py-1 text-sm text-sky-600 dark:bg-sky-600/20 dark:text-sky-400">
-        <span class="hidden sm:inline-block">October 2023</span>
-        <span class="sm:hidden">october-23</span>
+        <span class="hidden sm:inline-block">December 2023</span>
+        <span class="sm:hidden">december-23</span>
       </span>
       <span class="inline-flex items-center space-x-1 text-sm text-sky-600 dark:text-sky-400">
-        <span>Unified package installation command in sfp, any package one command!</span>
+        <span>sfp Validate to always use newest validation scratch org. </span>
         <chevron-right class="h-5 w-5" />
       </span>
     </a>
